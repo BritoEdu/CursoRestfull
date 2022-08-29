2123e83de3cf97e428412ad60d534e390425da56
Eduardo Brito <brito.edu20@gmail.com>
2022-08-29 20:07:51 -0300

    Commit inicio 3 aula

---
+++ 03_ApiCalculadora/CursoApiRestfull/CursoApiRestfull/Model/Person.cs
@@ -0,0 +1,11 @@
+﻿namespace CursoApiRestfull.Model
+{
+    public class Person
+    {
+        public long Id { get; set; }
+        public string Nome { get; set; } 
+        public string Sobrenome { get; set; }
+        public string Endereço { get; set; }
+        public string Genero { get; set; }
+    }
+}
